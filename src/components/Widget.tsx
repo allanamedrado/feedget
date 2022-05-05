4c178947e74464521741af6bc424cc25ec0cb25f
Allana Maria Medrado Araújo <allana.araujo@autotrac.com.br>
2022-05-05 13:15:39 -0300

    componentização de elementos - criado componentes do form de Feedback - organização de pastas

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,26 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
+import { Popover } from '@headlessui/react';
+import { WidgetForm } from './WidgetForm';
 
 //Estado (variavel): o react observa as variaveis do estado e sempre que ela muda cria a interface do componente de novo com o novo valor
 //setIsWidgetOpen é um função para alterar a variavel 
 
 export function Widget() {
-    const [ isWidgetOpen, setIsWidgetOpen ] = useState(false)
+    // const [ isWidgetOpen, setIsWidgetOpen ] = useState(false)
 
-    function toggleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen)
-    }
+    // function toggleWidgetVisibility() {
+    //     setIsWidgetOpen(!isWidgetOpen)
+    // }
 
     return (
-        <div className='absolute bottom-5 right-5'>
-            { isWidgetOpen ? <p>Hello World</p> : null }
+        <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
+            <Popover.Panel><WidgetForm /></Popover.Panel> 
 
-            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6"/>
 
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500'> <span className='pl-2'></span> Feedback</span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
